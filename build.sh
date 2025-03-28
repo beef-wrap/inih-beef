f58dec1c76a9a48c3e5a5357f0ed68ffc0316607
unknown <farismmk@gmail.com>
2025-03-28 03:13:47 -0700

    use xbuild

--- build.sh
+++
@@ -1,5 +0,0 @@
-cmake -S . -B inih/build -DCMAKE_BUILD_TYPE=Debug
-cmake --build inih/build --config Debug
-
-cmake -S . -B inih/build -DCMAKE_BUILD_TYPE=Release
-cmake --build inih/build --config Release
